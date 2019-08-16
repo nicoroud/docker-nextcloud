27a2bd9cd692c80fe4205c38169a201887ea4647
nicoroud <nicoroud@users.noreply.github.com>
2019-08-16 14:30:16 +0200

    Update version.sh

--- version.sh
+++ version.sh
@@ -1,6 +1,32 @@
 #!/bin/bash
-fullversions=( $( curl -fsSL 'https://download.nextcloud.com/server/releases/' |tac|tac| \
+
+# 16-08-2019
+# Nicolas Roudninski
+#
+
+opt=$1
+
+lastversions=( $( curl -fsSL 'https://download.nextcloud.com/server/releases/' |tac|tac| \
 	grep -oE 'nextcloud-[[:digit:]]+(\.[[:digit:]]+){2}' | \
 	grep -oE '[[:digit:]]+(\.[[:digit:]]+){2}' | \
 	sort -urV ) )
- echo $fullversions
+
+InstalledVersion()
+{
+	cat /usr/src/nextcloud/version.php | grep OC_VersionString | cut -d\' -f 2
+}
+
+while [ "$1" != "" ]; do
+	case $opt in 
+		-i)	shift
+			InstalledVersion
+			;;
+		-l)	shift
+			echo $lastversions
+			;;
+		*)
+			exit
+			;;
+	esac
+	shift
+done
