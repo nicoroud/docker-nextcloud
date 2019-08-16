bef1d08fe248f3948040eef0f6be45e62214e793
nicoroud <nicoroud@users.noreply.github.com>
2019-08-16 13:39:24 +0200

    Create version.sh

---
+++ version.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+fullversions=( $( curl -fsSL 'https://download.nextcloud.com/server/releases/' |tac|tac| \
+	grep -oE 'nextcloud-[[:digit:]]+(\.[[:digit:]]+){2}' | \
+	grep -oE '[[:digit:]]+(\.[[:digit:]]+){2}' | \
+	sort -urV ) )
+ echo $fullversions
